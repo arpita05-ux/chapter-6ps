45ce322de34a531b2585f1f366c047b8a30d7e0b
“Arpita <arpitakumari10653@gmail.com>
2025-05-30 10:29:56 +0530

    a program to find out whether a student has passed or failed

---
+++ 2_prob.py
@@ -0,0 +1,13 @@
+marks1 = int(input("Enter marks 1: "))
+marks2 = int(input("Enter marks 2: "))
+marks3 = int(input("Enter marks 3: "))
+
+# Check for total percentage
+total_percentage = 100*(marks1 + marks2 + marks3)/300
+
+if(total_percentage>=40):
+    print("You are pass")
+
+else:
+    print("You failed,try next year")
+
