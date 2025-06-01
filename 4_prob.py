7751c43673c9aa6209603d3e10f6f1d77123133c
“Arpita <arpitakumari10653@gmail.com>
2025-06-01 10:19:37 +0530

    a program to find whether a given username contains less than 10 characters or not

---
+++ 4_prob.py
@@ -0,0 +1,7 @@
+username = input("Enter username")
+
+if(len(username)<10):
+    print("Your username contain less than 10 character")
+
+else:
+    print("Your username contain more than 10 character")    
