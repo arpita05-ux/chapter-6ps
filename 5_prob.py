064c6872bcbb7ddaa31b832b0d481b094db676fb
“Arpita <arpitakumari10653@gmail.com>
2025-06-01 17:39:51 +0530

    a program which finds out whether a given name is present in a list or not

---
+++ 5_prob.py
@@ -0,0 +1,11 @@
+l = ["arpita" , "akul" , "ram" , "rohan"]
+
+name = input("Enter name: ")
+
+if(name in l):
+    print("Your name is in list")
+
+else:
+    print("Your name is not in list")    
+
+
