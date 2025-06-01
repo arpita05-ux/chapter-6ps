06a427ac10380b2867c45ae69683f2a12c978219
“Arpita <arpitakumari10653@gmail.com>
2025-06-01 20:26:41 +0530

    program to find out whether a given post is talking about “Harry” or not

---
+++ 7_prob.py
@@ -0,0 +1,7 @@
+statement = ('''This is a beautiful place.\nbut harry does not like it''')
+
+print(statement)  #this I add in additional
+if("harry" in statement):
+    print("Yes it is")
+else:
+    print("No it is not")    
